0e9c10f91009c4967c002915c750dc73629a17fd
Martin Vach <vades.cz@gmail.com>
2023-08-07 12:49:12 +0100

    Adding category content

--- source/_components/blog/category/index.blade.php
+++ source/_components/blog/category/index.blade.php
@@ -6,22 +6,26 @@
          {{ $page->description }}
     </div>
     @yield('content')
-
-    @foreach ($page->posts($posts) as $post)
-        <div class="flex flex-col mb-4">
-            <h2 class="text-3xl mt-0">
-                <a
-                    href="{{ $post->getUrl() }}"
-                    title="Read more - {{ $post->title }}"
-                    class="text-gray-900 font-extrabold"
-                >{{ $post->title }}</a>
-            </h2>
-        </div>
-
-        @if (! $loop->last)
-            <hr class="w-full border-b mt-2 mb-6">
-        @endif
-    @endforeach
+    <section class="articles">
+        @foreach ($page->posts($posts) as $post)
+            <article class="border-solid border-2 border-blue-500 p-4">
+                <figure class="posts-img">
+                    <a href="{{ $post->getPath() }}">
+                        <img src="{{ $post->img }}" alt="{{ $post->title }}">
+                    </a>
+                </figure>
+                <div class="posts-preview">
+                    <h2><a href="{{ $post->getPath() }}">{{ $post->title }}</a></h2>
+                    <div class="post-extra">
+                        <span class="posts-author">{{ $post->author}}</span>
+                        <span class="posts-date">{{ date('F j, Y', $post->date) }}</span>
+                    </div>
+                    <div class="posts-description">{{ $post->description}}</div>
+                    <a href="{{ $post->getPath() }}">Continue reading...</a>
+                </div>
+            </article>
+        @endforeach
+    </section>
 @endsection
 
 @section('sidebar')
