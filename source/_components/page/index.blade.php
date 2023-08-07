1cf885f125a4c095d7181ff2b62b72e1fc36106c
Martin Vach <vades.cz@gmail.com>
2023-08-07 13:31:19 +0100

    Adding page content

--- source/_components/page/index.blade.php
+++ source/_components/page/index.blade.php
@@ -1,11 +1,24 @@
 <x-layouts.master :page="$page">
     
 @section('main')
-    <h1>{{ $page->title }}</h1>
-    <div class="perex">
-         {{ $page->description }}
-    </div>
-    @yield('content')
+    <section class="border-solid border-2 border-green-500 p-4">
+        @if ($page->img ?? null)
+            <figure class="post-img">
+                    <img src="{{ $page->img }}" alt="{{ $page->title }}">
+            </figure>
+        @endif
+        <div>
+            <h1>{{ $page->title }}</h1>
+            <div class="perex">
+                {{ $page->description }}
+            </div>
+        </div>
+    </section>
+    <article class="border-solid border-2 border-blue-500 p-4">
+        @yield('content')
+    </article>
+    
+    
 @endsection
 
 @section('sidebar')
