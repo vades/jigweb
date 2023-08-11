bf92077854276bb65da4a915c9e04320378e8e93
Martin Vach <vades.cz@gmail.com>
2023-08-11 12:32:49 +0100

    Styling page

--- source/_components/page/index.blade.php
+++ source/_components/page/index.blade.php
@@ -1,27 +1,27 @@
 <x-layouts.master :page="$page">
     
 @section('main')
-    <section class="border-solid border-2 border-green-500 p-4">
+    <section class="mb-6">
         @if ($page->img ?? null)
-            <figure class="post-img">
-                    <img src="{{ $page->img }}" alt="{{ $page->title }}">
+            <figure class="mb-3">
+                <img class="w-24 border-4 border-slate-300" src="{{ $page->img }}" alt="{{ $page->title }}">
             </figure>
         @endif
         <div>
-            <h1>{{ $page->title }}</h1>
-            <div class="perex">
+            <h1 class="text-3xl text-gray-8600 mb-3">{{ $page->title }}</h1>
+            <div class="font-bold">
                 {{ $page->description }}
             </div>
         </div>
     </section>
-    <article class="border-solid border-2 border-blue-500 p-4">
+    <article class="">
         @yield('content')
     </article>
     
     
 @endsection
 
-@section('sidebar')
+{{-- @section('sidebar')
     <x-blog.sidebar :page="$page" :posts="$posts" :categories="$categories" />
-@endsection
+@endsection --}}
 </x-layouts.master>
