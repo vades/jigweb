ec29f56dbfc4e80a0d28de7022fba026ebbd9097
Martin Vach <vades.cz@gmail.com>
2023-09-11 14:15:05 +0100

    Adding pahe head component

--- source/_components/page/index.blade.php
+++ source/_components/page/index.blade.php
@@ -1,24 +1,10 @@
 <x-layouts.master :page="$page">
     
 @section('main')
-    <section class="flex flex-col md:flex-row md:items-start mb-3">
-        @if ($page->img ?? null)
-            <figure class="mb-3 md:mr-3">
-                <img class="w-48 border-4 border-skin-muted drop-shadow-lg" src="{{ $page->img }}" alt="{{ $page->title }}">
-            </figure>
-        @endif
-        <div>
-            <h1 class="text-3xl mb-3">{{ $page->title }}</h1>
-            <div class="font-bold">
-                {{ $page->description }}
-            </div>
-        </div>
-    </section>
+    <x-partials.page.head :page="$page"></x-partials.page.head>
     <article>
         @yield('content')
     </article>
-    
-    
 @endsection
 
 {{-- @section('sidebar')
